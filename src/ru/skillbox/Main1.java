708f27e3b250a54fa27d5864308957e497c215fb
Denis <bloguserprogrammer1@gmail.com>
2026-07-01 16:47:08 +0300

    My second commit1

--- src/ru/skillbox/Main1.java
+++ src/ru/skillbox/Main1.java
@@ -2,9 +2,7 @@ package ru.skillbox;
 
 public class Main1 {
     public static void main(String[] args) {
-        System.out.println("My first project");
-        System.out.println("Comment for my first commit!");
-
-        System.out.println(4 + 5);
+        System.out.println(4 + 5 + 79);
+        System.out.println("What is difference?");
     }
 }
