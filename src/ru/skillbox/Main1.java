d13ecc41b779a81dd19ca2651e10b720ce241636
Denis <bloguserprogrammer1@gmail.com>
2026-07-01 16:38:43 +0300

    1

---
+++ src/ru/skillbox/Main1.java
@@ -0,0 +1,8 @@
+package ru.skillbox;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("My first project");
+        System.out.println("Comment for my first commit!");
+    }
+}
